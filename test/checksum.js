d8a6cb76786ed5cfe8a5747b421b57c78cfa9fef
Chris Talkington <chris@talkingtontech.com>
2024-10-13 15:20:37 -0500

    esm: node v18+ required (#130)
    * conversion to esm
    
    * Update nodejs.yml
    
    * Update npmpublish.yml
    
    * Update npmpublish.yml

--- test/checksum.js
+++ test/checksum.js
@@ -1,59 +1,44 @@
+import { assert } from "chai";
+import crc32 from "crc-32";
+import { BinaryStream, DeadEndStream } from "./helpers/index.js";
+import CRC32Stream from "../lib/crc32-stream.js";
 /*global before,describe,it */
-
-'use strict';
-
-const {assert} = require('chai');
-const crc32 = require('crc-32');
-
-const {BinaryStream, DeadEndStream} = require('./helpers');
-
-const CRC32Stream = require('../lib/crc32-stream.js');
-
-describe('CRC32Stream', function() {
-  it('should checksum data while passing through data', function(done) {
-    const binary = new BinaryStream(1024 * 16);
-    const checksum = new CRC32Stream();
-    const deadend = new DeadEndStream();
-
-    checksum.on('end', function() {
-      assert.equal(checksum.digest().readUInt32BE(0), 3893830384);
-      assert.equal(checksum.digest('hex'), 'e81722f0');
-      assert.equal(checksum.hex(), 'E81722F0');
-      assert.equal(checksum.size(), 16384);
-      done();
+describe('CRC32Stream', function () {
+    it('should checksum data while passing through data', function (done) {
+        const binary = new BinaryStream(1024 * 16);
+        const checksum = new CRC32Stream();
+        const deadend = new DeadEndStream();
+        checksum.on('end', function () {
+            assert.equal(checksum.digest().readUInt32BE(0), 3893830384);
+            assert.equal(checksum.digest('hex'), 'e81722f0');
+            assert.equal(checksum.hex(), 'E81722F0');
+            assert.equal(checksum.size(), 16384);
+            done();
+        });
+        checksum.pipe(deadend);
+        binary.pipe(checksum);
     });
-
-    checksum.pipe(deadend);
-    binary.pipe(checksum);
-  });
-  
-  it('should have same checksum when bytes written together or separately', function(done) {
-    const checksum = new CRC32Stream();
-    const deadend = new DeadEndStream();
-    
-    const expectedChecksumValue = crc32.buf([157, 10, 217, 109, 100, 200, 300]) >>> 0;
-
-    checksum.on('end', function() {
-      assert.equal(checksum.digest().readUInt32BE(0), expectedChecksumValue);
-      done();
+    it('should have same checksum when bytes written together or separately', function (done) {
+        const checksum = new CRC32Stream();
+        const deadend = new DeadEndStream();
+        const expectedChecksumValue = crc32.buf([157, 10, 217, 109, 100, 200, 300]) >>> 0;
+        checksum.on('end', function () {
+            assert.equal(checksum.digest().readUInt32BE(0), expectedChecksumValue);
+            done();
+        });
+        checksum.write(Buffer.from([157, 10, 217, 109]));
+        checksum.write(Buffer.from([100, 200, 300]));
+        checksum.end();
+        checksum.pipe(deadend);
     });
-    
-    checksum.write(Buffer.from([157, 10, 217, 109]));
-    checksum.write(Buffer.from([100, 200, 300]));
-    checksum.end();
-    checksum.pipe(deadend);
-  });
-
-  it('should gracefully handle having no data chunks passed to it', function(done) {
-    const checksum = new CRC32Stream();
-    const deadend = new DeadEndStream();
-
-    checksum.on('end', function() {
-      assert.equal(checksum.digest().readUInt32BE(0), 0);
-      done();
+    it('should gracefully handle having no data chunks passed to it', function (done) {
+        const checksum = new CRC32Stream();
+        const deadend = new DeadEndStream();
+        checksum.on('end', function () {
+            assert.equal(checksum.digest().readUInt32BE(0), 0);
+            done();
+        });
+        checksum.pipe(deadend);
+        checksum.end();
     });
-
-    checksum.pipe(deadend);
-    checksum.end();
-  });
 });
--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,83 +1,59 @@
-'use strict';
+import crypto from "crypto";
+import { WriteStream } from "fs";
+import { Stream } from "stream";
+import { Readable, Writable } from "readable-stream";
 
-const crypto = require('crypto');
-const {WriteStream} = require('fs');
-
-const {Stream} = require('stream');
-const {Readable, Writable}  = require('readable-stream');
-
-function adjustDateByOffset(d, offset) {
-  d = (d instanceof Date) ? d : new Date();
-
-  if (offset >= 1) {
-    d.setMinutes(d.getMinutes() - offset);
-  } else {
-    d.setMinutes(d.getMinutes() + Math.abs(offset));
-  }
-
-  return d;
-}
-
-module.exports.adjustDateByOffset = adjustDateByOffset;
-
-function binaryBuffer(n) {
-  const buffer = Buffer.allocUnsafe(n);
-
-  for (let i = 0; i < n; i++) {
-    buffer.writeUInt8(i&255, i);
-  }
-
-  return buffer;
-}
-
-module.exports.binaryBuffer = binaryBuffer;
-
-class BinaryStream extends Readable {
-  constructor(size, options) {
-    super(size, options);
-
-    const buf = Buffer.allocUnsafe(size);
-
-    for (let i = 0; i < size; i++) {
-      buf.writeUInt8(i&255, i);
+export function adjustDateByOffset(d, offset) {
+    d = (d instanceof Date) ? d : new Date();
+    if (offset >= 1) {
+        d.setMinutes(d.getMinutes() - offset);
     }
-
-    this.push(buf);
-    this.push(null);
-  }
-
-  _read(size) {}
-}
-
-module.exports.BinaryStream = BinaryStream;
-
-class DeadEndStream extends Writable {
-  _write(chuck, encoding, callback) {
-    callback();
-  }
-}
-
-module.exports.DeadEndStream = DeadEndStream;
-
-class WriteHashStream extends WriteStream {
-  constructor(path, options) {
-    super(path, options);
-
-    this.hash = crypto.createHash('sha1');
-    this.digest = null;
-
-    this.on('close', function() {
-      this.digest = this.hash.digest('hex');
-    });
-  }
-
-  write(chunk) {
-    if (chunk) {
-      this.hash.update(chunk);
+    else {
+        d.setMinutes(d.getMinutes() + Math.abs(offset));
     }
-
-    return super.write(chunk);
-  }
+    return d;
 }
 
-module.exports.WriteHashStream = WriteHashStream;
+export function binaryBuffer(n) {
+    const buffer = Buffer.allocUnsafe(n);
+    for (let i = 0; i < n; i++) {
+        buffer.writeUInt8(i & 255, i);
+    }
+    return buffer;
+}
+
+export class BinaryStream extends Readable {
+    constructor(size, options) {
+        super(size, options);
+        const buf = Buffer.allocUnsafe(size);
+        for (let i = 0; i < size; i++) {
+            buf.writeUInt8(i & 255, i);
+        }
+        this.push(buf);
+        this.push(null);
+    }
+    _read(size) { }
+}
+
+export class DeadEndStream extends Writable {
+    _write(chuck, encoding, callback) {
+        callback();
+    }
+}
+
+export class WriteHashStream extends WriteStream {
+    constructor(path, options) {
+        super(path, options);
+        this.hash = crypto.createHash('sha1');
+        this.digest = null;
+        this.on('close', function () {
+            this.digest = this.hash.digest('hex');
+        });
+    }
+    write(chunk) {
+        if (chunk) {
+            this.hash.update(chunk);
+        }
+        return super.write(chunk);
+    }
+}
